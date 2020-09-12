dbdcb69d47fa585943fd341fcc5efef052ae8594
David Baas <Dcbaas@users.noreply.github.com>
2020-09-12 17:36:20 -0400

    init commit

---
+++ BudgetApp.Data/Models/Account.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BudgetApp.Data.Models
+{
+    public enum AccountType
+    {
+        CHECKING,
+        SAVINGS
+    }
+
+    public class Account
+    {
+        public long Id { get; set; }
+        public User UserName { get; set; }
+        public AccountType AccountType { get; set; }
+        public decimal AccountBalance { get; set; }
+    }
+}
---
+++ BudgetApp.Data/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace BudgetApp.Data.Models
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string UserName { get; set; }   
+        public decimal TotalBalance { get; set; }
+        public List<Account> UserAccounts { get; set; }
+    }
+}
